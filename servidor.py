6cf51172d315d23d14c65af350d6337705f5540d
Franciele Oliveira <32960110+fran-olv@users.noreply.github.com>
2023-10-19 21:08:43 -0300

    Update servidor.py

--- servidor.py
+++ servidor.py
@@ -1,29 +1,49 @@
 from socket import *
-
+import ssl
 
 
 def iniciar_servidor():
-    host = 'localhost'
+    host = "localhost"
     porta = 12001
+
+    caminho_certificado= ".ssl/certificado_servidor.crt"
+    caminho_chave=".ssl/chave_servidor.key"
 
     servidor = socket(AF_INET, SOCK_STREAM)
     servidor.bind((host, porta))
     servidor.listen(2)
 
-    print("Servidor HTTPS está ouvindo em https://localhost:12001")
+    print("Servidor de pé! ")
 
     while True:
         cliente_socket, endereco_cliente = servidor.accept()
 
-        try:
-            while cliente_socket:
-                mensagem = cliente_socket.recv(1024)
+        servidor_ssl = ssl.wrap_socket(
+            cliente_socket,
+            server_side=True,
+            certfile="./ssl/certificado_servidor.crt",
+            keyfile="./ssl/chave_servidor.key",
+            ssl_version= ssl.PROTOCOL_TLSv1
+        )
 
+
+
+        try:
+            print("Conexão segura estabelecida:")
+            print(f"Protocolo: {servidor_ssl.version()}")
+            print(f"Cifra: {servidor_ssl.cipher()}")
+
+            while servidor_ssl:
+                mensagem = servidor_ssl.read()
                 if mensagem:
-                    print("Mensagem recebida do cliente: {mensagem.decode('utf-8')}")
+                    print(f"Mensagem recebida do cliente: {mensagem.decode('utf-8')}")
+
+        except ssl.SSLError as e:
+            print(f"Erro SSL: {e}")
         except Exception as e:
-            print("Erro ao lidar com a mensagem: {e}")
+            print(f"Erro ao lidar com a mensagem: {e}")
         finally:
+            servidor_ssl.close()
             cliente_socket.close()
 
 if __name__ == "__main__":
