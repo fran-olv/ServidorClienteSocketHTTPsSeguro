e255de598133a057b68d55454d8723b66364e532
Franciele Oliveira <32960110+fran-olv@users.noreply.github.com>
2023-10-19 21:08:24 -0300

    Update cliente.py com certificado de seguranca

--- cliente.py
+++ cliente.py
@@ -1,21 +1,32 @@
 from socket import *
+import ssl
 
 
 def conectar_cliente():
-    host = 'localhost'
+    host = "localhost"
     porta = 12001
 
+    caminho_certificado_cliente = "./ssl/certificado_cliente.crt"
+    caminho_chave_cliente = "./ssl/chave_cliente.key"
 
     cliente = socket(AF_INET, SOCK_STREAM)
-    cliente.connect((host,porta))
+    cliente_ssl= ssl.wrap_socket(cliente, ca_certs="./ssl/certificado_cliente.crt")
 
-    while True:
-        mensagem = input("Escreva sua mensagem:> ")
+    try:
+        cliente_ssl.connect((host, porta))
+        print("Conexão HTTPS segura estabelecida:")
+        print(f"Protocolo: {cliente_ssl.version()}")
+        print(f"Cifra: {cliente_ssl.cipher()}")
 
-        if mensagem:
-            cliente.send(mensagem.encode())
+        while True:
+            mensagem = input("Escreva sua mensagem: ")
+            if mensagem:
+                cliente_ssl.send(mensagem.encode())
+    except Exception as e:
+        print(f"Erro na conexão: {e}")
+    finally:
+        cliente_ssl.close()
 
-    clinte.close()
 
 if __name__ == "__main__":
     conectar_cliente()
